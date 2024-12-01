0e58573e995b3718343a2cfe529ac7ff7473c739
KonovalovDM <kdm232006@gmail.com>
2024-12-01 13:55:52 +0300

    fixt some code

--- app/routes.py
+++ app/routes.py
@@ -47,8 +47,8 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            print(f"Redirecting to: {next_page or url_for('main.home')}")
-            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+            print(f"Redirecting to: {next_page or url_for('main.account')}")
+            return redirect(next_page) if next_page else redirect(url_for('main.account'))
         else:
             flash('Введены неверные данные для входа.', 'danger')
     return render_template('login.html', form=form)
